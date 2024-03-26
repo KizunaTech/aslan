b42d691defe81c3367058d2eb85f3382778bc8ee
JeremyS-T <jtanrev@gmail.com>
2024-03-26 21:48:33 +0800

    Update database scheme from sqlite3 to postgreSQL

--- db/migrate/20240326070139_create_posts.rb
+++ db/migrate/20240326070139_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts, id: :uuid, default: -> { "gen_random_uuid()" } do |t|
       t.text :content
-      t.references :user, foreign_key: true
+      t.references :user, type: :uuid, foreign_key: true
 
       t.timestamps
     end
