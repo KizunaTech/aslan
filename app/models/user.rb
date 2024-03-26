83abd55382e09814167f2aa2e2033f409ceaf617
JeremyS-T <jtanrev@gmail.com>
2024-03-26 15:16:12 +0800

    Execute User Model migrations, Initialize Post Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :saved_posts
+  has_many :posts, through: :saved_posts
+
+  def saved_posts_count
+    saved_posts.count
+  end
 end
