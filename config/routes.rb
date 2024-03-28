db01d00bb108eb9cfd8aad29853317f5b410a7d4
JeremyS-T <jtanrev@gmail.com>
2024-03-28 09:03:48 +0800

    Initialize delete function

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/posts/compose', to: "posts#new", as: :new_post
   get '/posts/:id', to: "posts#show", as: :post
   patch '/posts/:id', to: "posts#update"
+  delete '/posts/:id', to: "posts#destroy"
   get '/posts/:id/edit', to: "posts#edit", as: :edit_post
   post '/posts', to: "posts#create", as: :posts
 
