b16380e50f8d62de62e81736f74c2168807a1e2a
Vincent Hong <vinlegend1@gmail.com>
2024-04-18 06:44:43 +0800

    initial files for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,27 @@
 class PostsController < ApplicationController
-  def post
-  end
+    before_action :authenticate_user!
+
+    def index
+        @posts = Post.all.order("created_at DESC")
+        puts @posts.inspect
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = current_user.posts.build(post_params)
+        if @post.save
+            redirect_to @post, notice: 'Post was successfully created.'
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :body, :verse)
+    end
 end
