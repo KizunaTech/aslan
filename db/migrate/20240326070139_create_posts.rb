1f595c7bc1912b9de24b62f9a30653ad1a97021b
BaronViper <jtanrev@gmail.com>
2024-04-25 20:22:46 +0800

    Merge similar migrations

--- db/migrate/20240326070139_create_posts.rb
+++ db/migrate/20240326070139_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts, id: :uuid, default: -> { "gen_random_uuid()" } do |t|
+      t.string :title
+      t.text :body
+      t.string :verse
       t.text :content
       t.references :user, type: :uuid, foreign_key: true
 
